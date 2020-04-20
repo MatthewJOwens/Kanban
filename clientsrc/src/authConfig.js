437ff99f9d57abc93a122152f0877387d5452ad0
Nate Sherfick <natesherfick@gmail.com>
2020-04-20 14:29:21 -0600

    init

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "dev-zty12rnj.auth0.com";
+export const audience = "https://kanban.com";
+export const clientId = "4kHmPqS4ciQhSCPNG7tDNrVXg47ozpHU";
