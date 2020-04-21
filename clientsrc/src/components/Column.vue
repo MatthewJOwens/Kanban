62d8df68bec81d0573e2097005826363099c904c
MatthewJOwens <mjowens@mail.com>
2020-04-21 10:46:48 -0600

    Started writing columns, forgot to write back end

---
+++ clientsrc/src/components/Column.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="columnComponent p-2 mb-2 shadow rounded">
+    <!-- <div class="col-3"></div> -->
+    <p>Column Component loaded.</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "columnComponent",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+.columnComponent {
+  min-width: 350px;
+}
+</style>
--- clientsrc/src/views/Board.vue
+++ clientsrc/src/views/Board.vue
@@ -2,14 +2,33 @@
   <div class="board">
     <h1 v-if="board.title">{{board.title}}</h1>
     <h1 v-else>Loading...</h1>
-    <p>{{board}}</p>
-    <p>This is a board.</p>
+    <div class="d-flex flex-row items-start">
+      <Column
+        v-for="column in columns"
+        :key="column.id"
+        :columnData="column"
+      >The column component has not loaded.</Column>
+      <div v-if="showColumn">
+        <input type="text" v-model="newColumn.title" placeholder="Column title" />
+      </div>
+      <div v-else @click="showColumn = !showColumn">
+        <p>Add Column</p>
+        <button class="btn btn-primary" @submit.prevent="addColumn()" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Column from "../components/Column.vue";
 export default {
   name: "board",
+  data() {
+    return {
+      showColumn: false,
+      newColumn: {}
+    };
+  },
   computed: {
     board() {
       //FIXME This does not work on page reload because the activeBoard is empty in the store
@@ -22,6 +41,17 @@ export default {
   created() {
     this.$store.dispatch("getProfile");
   },
-  props: ["boardId"]
+  methods: {
+    newColumn() {
+      // TODO Finish writing method to add Column after writing back end
+      // this.newColumn.
+      this.$store.dispatch("addColumn", this.newColumn);
+      // TODO need to add to store
+    }
+  },
+  props: ["boardId"],
+  components: {
+    Column
+  }
 };
 </script>
