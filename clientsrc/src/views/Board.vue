4a279a93925a88e205032b79729dcb47247a4fe3
MatthewJOwens <mjowens@mail.com>
2020-04-25 18:28:05 -0600

    a couple formatting changes

--- clientsrc/src/views/Board.vue
+++ clientsrc/src/views/Board.vue
@@ -1,29 +1,36 @@
 <template>
-<div class="bg-fuji fullheight">
-  <div class="board">
-    <!-- //TODO fix resizing issue with modal -->
-    <h1 v-if="!editing" class="margin-board-top mb-3" ><span @click="editing = !editing">{{board.title}}</span></h1>
-    <div v-else class="margin-board-top mb-3" >
-      <input v-model="board.title" @keyup.enter="editBoardTitle()"/>
-      <small>
-        <span @click.stop="deleteBoard(board.id)" class="far fa-trash-alt text-danger"></span>
-      </small>
-    </div>
-    <div class="d-flex flex-row items-start">
-      <List class="bg-card" v-for="list in lists" :key="list.id" :listData="list">The list component has not loaded.</List>
-      <div class="min-width-272 p-2 shadow rounded m-2 navy-font">
-        <div v-if="showList">
-          <input
-            type="text"
-            v-model="newList.title"
-            placeholder="List title"
-            @keyup.enter="addList()"
-          />
+  <div class="bg-fuji fullheight">
+    <div class="board">
+      <!-- //TODO fix resizing issue with modal -->
+      <h1 v-if="!editing" class="margin-board-top mb-3">
+        <span @click="editing = !editing">{{board.title}}</span>
+      </h1>
+      <div v-else class="margin-board-top mb-3">
+        <input v-model="board.title" @keyup.enter="editBoardTitle()" />
+        <small>
+          <span @click.stop="deleteBoard(board.id)" class="far fa-trash-alt text-danger"></span>
+        </small>
+      </div>
+      <div class="d-flex flex-row items-start">
+        <List
+          class="bg-card"
+          v-for="list in lists"
+          :key="list.id"
+          :listData="list"
+        >The list component has not loaded.</List>
+        <div class="min-width-272 p-2 shadow rounded m-2 navy-font">
+          <div v-if="showList">
+            <input
+              type="text"
+              v-model="newList.title"
+              placeholder="List title"
+              @keyup.enter="addList()"
+            />
+          </div>
+          <div @click="showList = !showList" v-else>+ Add List</div>
         </div>
-        <div class="" @click="showList = !showList" v-else>+ Add List</div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -62,10 +69,12 @@ export default {
       this.newList.boardId = this.board.id;
       this.$store.dispatch("addList", this.newList);
       this.showList = false;
-      newList.title = {}
+      newList.title = "";
     },
     editBoardTitle() {
-      if (this.board.title == "") {this.board.title = "Untitled"}
+      if (this.board.title == "") {
+        this.board.title = "Untitled";
+      }
       this.$store.dispatch("editBoardTitle", this.board);
       this.editing = false;
     },
@@ -88,17 +97,15 @@ export default {
   min-width: 272px;
   height: 100%;
 }
-.margin-board-top{
+.margin-board-top {
   padding-top: 15vh;
 }
 /* @onmouseover and v-show */
 .bg-card {
-  background-color: rgba(255, 255, 255, 0.5)!important;
-
+  background-color: rgba(255, 255, 255, 0.5) !important;
 }
-.navy-font{
+.navy-font {
   color: #2c3e50;
-    background-color: rgba(255, 255, 255, 0.5)!important;
-
+  background-color: rgba(255, 255, 255, 0.5) !important;
 }
 </style>
